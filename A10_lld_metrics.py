49d9713407a7fa99a1570842a756576f3d4d278c
Artur Brasil <arturbrasil@gmail.com>
2018-10-02 15:07:54 -0300

    Create A10_lld_metrics.py

---
+++ A10_lld_metrics.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python2.7
+from bs4 import BeautifulSoup
+import re
+import json
+import requests
+import urllib3
+from lxml import html
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+urllib3.disable_warnings()
+
+payload = {
+        "username": "admin",
+        "password": "BackBone1000"
+}
+
+session_requests = requests.session()
+login_url = "https://10.69.36.26/gui/auth/login/"
+result = session_requests.get(login_url, verify=False)
+
+result = session_requests.post(
+        login_url, 
+        data = payload, 
+        headers = dict(referer=login_url),
+        verify=False
+)
+
+url = 'https://10.69.36.26/gui/dashboard/adc/attackprevention/'
+result = session_requests.get(
+        url, 
+        headers = dict(referer = url),
+        verify=False
+)
+
+content = result.text
+soup = BeautifulSoup(content, "html.parser")
+data = []
+items = soup.findAll('div', class_="summary__content-item")
+for item in items:
+  result = item.get_text().rstrip()
+  (pkey, pvalue) = result.split(':')
+  fkey  = re.findall('\w+', pkey)
+  fvalue  = re.findall('\d+', pvalue)
+  key = ' '.join(str(x) for x in fkey)
+  value = ' '.join(str(x) for x in fvalue)
+  data.append({ '{#METRIC}' : key })
+print json.dumps({"data": data})
+
